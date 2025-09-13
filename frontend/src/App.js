5f70a22097601ea2b55eff058f541f552dfb8881
Eddie Huynh <eddhuynh@gmail.com>
2025-09-13 16:19:42 -0400

    Updating Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+// src/App.js
+
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function App(){
+  const [message, setMessage] = React.useState('')
+
+  useEffect(() => {
+    axios.get('/api/test')
+    .then(response =>{
+      setMessage(response.data.message)
+    })
+    .catch(error => {
+      console.error("there was an error with fetching data", error)
+    })
+  }, [])
+  return(
+    <div>
+      <h1>My Task Tracker</h1>
+      <p>Message from server: <strong>{message}</strong></p>
+    </div>
+  )
+}
+
+export default App
